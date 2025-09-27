cfada937239b73bd0729a1f86aba74d3a4331f03
GOMOPRO777 <GOMOPRO777@DESKTOP-PDL0G17>
2025-09-27 18:04:24 +0300

    Add project files.

---
+++ Tyuiu.KhafizovTV.Sprint1.Task0.V11.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.KhafizovTV.Sprint1.Task0.V11.Lib;
+
+namespace Tyuiu.KhafizovTV.Sprint1.Task0.V29.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            var res = ds.Calculate();
+            Assert.AreEqual(7, res);
+        }
+    }
+}
