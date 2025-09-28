70cda7f43537e8006bab8a3b1c492af252783b9d
GOMOPRO777 <GOMOPRO777@DESKTOP-PDL0G17>
2025-09-28 18:46:23 +0300

    Task4

---
+++ Tyuiu.KhafizovTV.Sprint1.Task4.V19.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.KhafizovTV.Sprint1.Task4.V19.Lib;
+
+namespace Tyuiu.KhafizovTV.Sprint1.Task4.V19.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 3;
+            double y = 5;
+            double wait = 2.828;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
