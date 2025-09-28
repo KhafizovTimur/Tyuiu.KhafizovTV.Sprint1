feeb255a9711537e81426f9b0c090a08626875c3
GOMOPRO777 <GOMOPRO777@DESKTOP-PDL0G17>
2025-09-28 18:30:25 +0300

    Task3

---
+++ Tyuiu.KhafizovTV.Sprint1.Task3.V5.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.KhafizovTV.Sprint1.Task3.V5.Lib;
+
+namespace Tyuiu.KhafizovTV.Sprint1.Task3.V5.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double k = 120;
+            double s = 3.5;
+            double wait = 420.000;
+            var res = ds.DistanceLength(k, s);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.KhafizovTV.Sprint1.Task3.V5/Program.cs
@@ -0,0 +1,41 @@
+﻿using Tyuiu.KhafizovTV.Sprint1.Task3.V5.Lib;
+
+namespace Tyuiu.KhafizovTV.Sprint1.Task3.V5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнил: Хафизов Т. В. | ИСПб-25-1";
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* Спринт #1                                                              *");
+            Console.WriteLine("* Тема: Операторы составного присваивания                                *");
+            Console.WriteLine("* Задание #3                                                             *");
+            Console.WriteLine("* Вариант #5                                                             *");
+            Console.WriteLine("* Выполнил: Хафизов Т. В. | ИСПб-25-1                                    *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                               *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные,*");
+            Console.WriteLine("* выполняет указанные расчёты и печатает результат на экране.           	*");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                       *");
+            Console.WriteLine("**************************************************************************");
+
+            Console.Write("Введите масштаб карты (км в 1 см): ");
+            double k = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите расстояние между точками, изображающими населенные пункты (см): ");
+            double s = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                             *");
+            Console.WriteLine("**************************************************************************");
+
+            double result = ds.DistanceLength(k, s);
+            Console.WriteLine($"Расстояние между населенными пунктами = {result:F3}");
+            Console.ReadKey();
+
+        }
+    }
+}
