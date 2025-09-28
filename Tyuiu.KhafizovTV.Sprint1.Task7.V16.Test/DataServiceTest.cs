c3ba7b32b3a5e0dc31452204edadf2de51549119
GOMOPRO777 <GOMOPRO777@DESKTOP-PDL0G17>
2025-09-28 20:48:26 +0300

    Task7

---
+++ Tyuiu.KhafizovTV.Sprint1.Task7.V16.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.KhafizovTV.Sprint1.Task7.V16.Lib;
+
+namespace Tyuiu.KhafizovTV.Sprint1.Task7.V16.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 100;
+            double wait = 0.004;
+            var res = ds.Calculate(x);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.KhafizovTV.Sprint1.Task7.V16/Program.cs
@@ -0,0 +1,45 @@
+﻿using Tyuiu.KhafizovTV.Sprint1.Task7.V16.Lib;
+
+namespace Tyuiu.KhafizovTV.Sprint1.Task7.V16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнил: Хафизов Т. В. | ИСПб-25-1";
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* Спринт #1                                                              *");
+            Console.WriteLine("* Тема: Добавление к решению итоговых проектов по спринту                *");
+            Console.WriteLine("* Задание #7                                                             *");
+            Console.WriteLine("* Вариант #16                                                            *");
+            Console.WriteLine("* Выполнил: Хафизов Т. В. | ИСПб-25-1                                    *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                               *");
+            Console.WriteLine("* Написать программу, которая вычисляет математическое выражение         *");
+            Console.WriteLine("* по исходным значениям данных, вводимых пользователем.       	      	*");
+            Console.WriteLine("*                                                                        *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                       *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("                                                                          ");
+            Console.WriteLine("                     cosx^2                                               ");
+            Console.WriteLine("z = sin(sqrt(x^2)) + ------ - sin(sqrt(x^2-1))                            ");
+            Console.WriteLine("                      3x^3                                                ");
+            Console.WriteLine("                                                                          ");
+
+            double x;
+            Console.WriteLine("Введите значение x:");
+            x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                             *");
+            Console.WriteLine("**************************************************************************");
+
+            Console.WriteLine("z = " + $"{ds.Calculate(x):F3}");
+
+            Console.ReadKey();
+        }
+    }
+}
